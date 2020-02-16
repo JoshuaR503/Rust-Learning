0c6845171014a2a389ce1771f9339d8fe4456593
Joshua Garcia <josshuagarciia000@gmail.com>
2020-02-15 21:16:10 -0800

    added shadowing

---
+++ 4_shadowing/readme.md
@@ -0,0 +1,7 @@
+# Rust Learning - Shadowing
+
+[Documentation](https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing)
+
+You can declare a new variable with the same name as a previous variable, and the new variable shadows the previous variable. Rustaceans say that the first variable is shadowed by the second, which means that the second variable’s value is what appears when the variable is used.
+
+Shadowing is different from marking a variable as ```mut```.
--- 4_shadowing/src/main.rs
+++ 4_shadowing/src/main.rs
@@ -1,3 +1,13 @@
+// This program first binds x to a value of 5. 
+// Then it shadows x by repeating let x =, taking the original value and adding 1 so the value of x is then 6. 
+// The third let statement also shadows x, multiplying the previous value by 2 to give x a final value of 12. 
+// When we run this program, it will output the following:
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
 }
